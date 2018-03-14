309674cbbcf06747399e2fc16b58f411336d01e7
Ayush <37358635+ayush1120@users.noreply.github.com>
2018-03-14 11:54:53 +0530

    model done
    Co-Authored-By: akhilran <akhilran@users.noreply.github.com>

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from home.models import Component, ComponentRequest, UserProfileInfo
 
 # Register your models here.
+admin.site.register(Component)
+admin.site.register(ComponentRequest)
+admin.site.register(UserProfileInfo)
--- home/models.py
+++ home/models.py
@@ -9,7 +9,16 @@ class UserProfileInfo(models.Model):
     component_request = models
 
 
+class Component(models.Model):
+    ComponentName = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.ComponentName
+
+
 class ComponentRequest(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField()
+    mobileno = models.BigIntegerField()
+    component = models.ForeignKey('Component', on_delete=models.CASCADE)
