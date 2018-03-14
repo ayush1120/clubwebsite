55d1529e325601956a58839706528347e32f66e3
Ayush <37358635+ayush1120@users.noreply.github.com>
2018-03-14 11:11:00 +0530

    models.py
    Co-Authored-By: akhilran <akhilran@users.noreply.github.com>

--- home/models.py
+++ home/models.py
@@ -6,3 +6,10 @@ from django.contrib.auth.models import User
 class UserProfileInfo(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    component_request = models
+
+
+class ComponentRequest(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
